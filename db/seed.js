f2d0e51486cfa3b059aa8c2a7b7aa03f07492363
stanleytiu7 <stanleytiu7@gmail.com>
2017-07-29 15:15:48 -0400

    commit for most up to date seed file

--- db/seed.js
+++ db/seed.js
@@ -5,18 +5,55 @@ const db = require('APP/db'),
   { mapValues } = require('lodash')
 
 function seedEverything() {
-  console.log('badbye')
   const seeded = {
     users: users(),
     products: products(),
-    addresses: addresses(),
     categories: categories(),
     orders: orders()
   }
 
+  seeded.addresses = addresses(seeded)
+  seeded.reviews = reviews(seeded)
+
   return Promise.props(seeded)
 }
-
+const reviews = seed(Review,({users, products})=> ({
+  reviewA: {
+  title: 'Loren Ipsum',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at augue semper, mollis urna eget, dictum mi. Etiam elit mi, commodo pharetra elit consequat, mollis rutrum ipsum. Nullam aliquet quam vitae congue facilisis. Morbi quis mattis diam, semper varius purus. Aliquam pellentesque iaculis varius. Fusce quis venenatis libero. Sed eget pellentesque arcu, sed vulputate leo. Ut venenatis dui egestas mauris pharetra, non placerat libero dignissim. In hac habitasse platea dictumst. Aliquam elit quam, tristique eu lacus quis, bibendum imperdiet dolor. Pellentesque lobortis dapibus porta. Vestibulum mollis turpis non turpis ullamcorper, vitae egestas lacus ullamcorper. Quisque nec ipsum magna. Quisque non ex sagittis, accumsan lacus nec, mollis diam. Nunc dapibus pretium justo quis facilisis. Praesent egestas velit lacinia elit sodales, vitae tincidunt nisl cursus.',
+  rating: 2,
+  product_id: products.productA.id,
+  user_id: users.userA.id
+  },
+reviewB: {
+  title: 'Somethign',
+  description: 'ssomethinsomethinsomethinsomethinsomethinsomethinsomethinsomethinsomethinsomethinsomethinsomethinsomethinsomethinsomethinsomethinsomethinsomethinsomethinsomethinsomethinsomethinsomethinsomethinsomethinsomethinsomethinsomethinsomethinsomethinsomethinsomethinsomethinsomethinsomethinsomethinsomethinsomethinsomethinsomethinsomethinsomethinsomethinsomethinsomethinsomethinsomethinsomethinsomethinsomethinsomethinsomethinsomethinsomethinsomethinsomethinsomethinsomethinsomethinsomethinsomethinsomethinsomethinsomethinsomethinsomethinsomethinsomethinomethin',
+  rating: 4,
+  product_id: products.productB.id,
+  user_id: users.userB.id
+  },
+reviewC: {
+  title: 'hello',
+  description: 'hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello',
+  rating: 5,
+  product_id: products.productC.id,
+  user_id: users.userC.id
+  },
+reviewD: {
+  title: 'mate',
+  description: 'mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate mate',
+  rating: 3,
+  product_id: products.productD.id,
+  user_id: users.userD.id
+  }, 
+reviewE: {
+  title: 'Who let the dogs out?',
+  description: 'who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who? who?',
+  rating: 1,
+  product_id: products.productE.id,
+  user_id: users.userE.id
+  }
+  }))
 const users = seed(User, {
   userA: {
     firstName: 'Jamie',
@@ -105,13 +142,15 @@ const products = seed(Product, {
   },
 })
 
-const addresses = seed(Address, {
+const addresses = seed(Address,
+  ({ users }) => ({
   addressA: {
     street: '123 Fake Street',
     city: 'Fake City',
     zip: '12345',
     state: 'Maryland',
-    country: 'FakeLand somewhere Fake'
+    country: 'FakeLand somewhere Fake',
+    user_id: users.userA.id
   },
   addressB: {
     street: 'Something Something Broadway',
@@ -119,28 +158,33 @@ const addresses = seed(Address, {
     city: 'New York',
     zip: '10004',
     state: 'New York',
-    country: 'United States'
+    country: 'United States',
+    user_id: users.userB.id
   },
   addressC: {
     street: '1 Bikini Bottom',
     city: 'Bikini Bottom',
     zip: '12914',
     state: 'Tennessee',
-    country: 'UnderWaterLand'
+    country: 'UnderWaterLand',
+    user_id: users.userC.id
+
   },
   addressD: {
     street: '5 Power rangers',
     city: 'South Dakota',
     zip: '29193',
-    state: 'Alabama'
+    state: 'Alabama',
+    user_id: users.userD.id
   },
   addressE: {
     street: '21 David Yoon Court',
     city: 'GeorgiaCity',
     zip: '92810',
-    state: 'Georgia'
-  },
-})
+    state: 'Georgia',
+    user_id: users.userE.id
+  }
+}))
 
 const categories = seed(Category, {
   categoryA: {
@@ -203,79 +247,65 @@ const orders = seed(Order, {
   },
 })
 
-const reviews = seed(Review, {
-  reviewA: {
-    title: 'Wow what a great product!',
-    description: 'I really love the feel of real mermaid skin against my own skin. Faux mermaid skin makes my skin feel scratchy. Quality is nice, ordered and got my item within 20 days. 5 stars from me.',
-    rating: 5
-  },
-  reviewB: {
-    title: 'WORST PRODUCT EVER',
-    description: 'Ask yourself this: Do I REALLY need a lobster shell necklace? Of COURSE I do. I can\'t believe that anyone would need to ask themselves this question when in fact it is exactly that! My Lobster shell necklace broke a couple days later. They get a solid 1/5 from me',
-    rating: 1
-  },
-  reviewC: {
-    title: 'Can I even find this anywhere else?',
-    description: 'When I look to buy something that is weird and a horrible gift, Ocean Spray is the first thing that comes to mind! All the gifts here are magnificently and horribly crafted with detail and negligence. Quite frankly, I\'m not even sure if I know why I love or hate this store so much!',
-    rating: 3
-  },
-  reviewD: {
-    title: 'pretty good',
-    description: 'cool items',
-    rating: 3
-  },
-  reviewE: {
-    title: 'Mom is allergic to swordfish',
-    description: 'After buying a gift for my mother for Mother\'s Day, she swelled up to the size of a blueberry after she put her authentic \'Seaweed\' brooch on. The only seafood she is allergic to is swordfish. I am going to sue this company for the hospital and legal fees.',
-    rating: 1
-  },
-  reviewF: {
-    title: 'I am low-vision impaired and I accessed this site by tabbing',
-    description: 'I am so glad that your website is handicapped enabled. All my favorite websites are so hard to use but not this one!',
-    rating: 5
-  },
-})
+/*
+ *const reviews = seed(Review, {
+ *  reviewA: {
+ *    title: 'Wow what a great product!',
+ *    description: 'I really love the feel of real mermaid skin against my own skin. Faux mermaid skin makes my skin feel scratchy. Quality is nice, ordered and got my item within 20 days. 5 stars from me.',
+ *    rating: 5
+ *  },
+ *  reviewB: {
+ *    title: 'WORST PRODUCT EVER',
+ *    description: 'Ask yourself this: Do I REALLY need a lobster shell necklace? Of COURSE I do. I can\'t believe that anyone would need to ask themselves this question when in fact it is exactly that! My Lobster shell necklace broke a couple days later. They get a solid 1/5 from me',
+ *    rating: 1
+ *  },
+ *  reviewC: {
+ *    title: 'Can I even find this anywhere else?',
+ *    description: 'When I look to buy something that is weird and a horrible gift, Ocean Spray is the first thing that comes to mind! All the gifts here are magnificently and horribly crafted with detail and negligence. Quite frankly, I\'m not even sure if I know why I love or hate this store so much!',
+ *    rating: 3
+ *  },
+ *  reviewD: {
+ *    title: 'pretty good',
+ *    description: 'cool items',
+ *    rating: 3
+ *  },
+ *  reviewE: {
+ *    title: 'Mom is allergic to swordfish',
+ *    description: 'After buying a gift for my mother for Mother\'s Day, she swelled up to the size of a blueberry after she put her authentic \'Seaweed\' brooch on. The only seafood she is allergic to is swordfish. I am going to sue this company for the hospital and legal fees.',
+ *    rating: 1
+ *  },
+ *  reviewF: {
+ *    title: 'I am low-vision impaired and I accessed this site by tabbing',
+ *    description: 'I am so glad that your website is handicapped enabled. All my favorite websites are so hard to use but not this one!',
+ *    rating: 5
+ *  },
+ *})
+ */
 
-//const addressRelations = seed(Address,
-//({users}) => ({
-//'addressA': {
-//user_id: users.userA.id
-//},
-//'address
-//})
-
-//const favorites = seed(Favorite,
-//// We're specifying a function here, rather than just a rows object.
-//// Using a function lets us receive the previously-seeded rows (the seed
-//// function does this wiring for us).
-////
-//// This lets us reference previously-created rows in order to create the join
-//// rows. We can reference them by the names we used above (which is why we used
-//// Objects above, rather than just arrays).
-//({users, things}) => ({
-//// The easiest way to seed associations seems to be to just create rows
-//// in the join table.
-//'obama loves surfing': {
-//user_id: users.barack.id,    // users.barack is an instance of the User model
-//// that we created in the user seed above.
-//// The seed function wires the promises so that it'll
-//// have been created already.
-//thing_id: things.surfing.id  // Same thing for things.
-//},
-//'god is into smiting': {
-//user_id: users.god.id,
-//thing_id: things.smiting.id
-//},
-//'obama loves puppies': {
-//user_id: users.barack.id,
-//thing_id: things.puppies.id
-//},
-//'god loves puppies': {
-//user_id: users.god.id,
-//thing_id: things.puppies.id
-//},
-//})
-//)
+/*
+ *const favorites = seed(Favorite,
+ *  ({
+ *    users,
+ *    things
+ *  }) => ({
+ *    'obama loves surfing': {
+ *      user_id: users.barack.id, // users.barack is an instance of the User model
+ *      thing_id: things.surfing.id // Same thing for things.
+ *    },
+ *    'god is into smiting': {
+ *      user_id: users.god.id,
+ *      thing_id: things.smiting.id
+ *    },
+ *    'obama loves puppies': {
+ *      user_id: users.barack.id,
+ *      thing_id: things.puppies.id
+ *    },
+ *    'god loves puppies': {
+ *      user_id: users.god.id,
+ *      thing_id: things.puppies.id
+ *    },
+ *  }))
+ */
 
 if (module === require.main) {
   db.didSync
@@ -350,4 +380,11 @@ function seed(Model, rows) {
   }
 }
 
-module.exports = Object.assign(seed, { users, addresses, orders, categories, products, reviews })
+module.exports = Object.assign(seed, {
+  users,
+  addresses,
+  orders,
+  categories,
+  products,
+  reviews
+})
